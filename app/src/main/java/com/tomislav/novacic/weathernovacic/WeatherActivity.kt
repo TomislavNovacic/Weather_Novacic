bca378eb660abc6bcce740fe91c3089caca77c91
TomislavNovačić <tomislav.novacic2@gmail.com>
2020-09-12 17:21:58 +0200

    Renamed MainActivity to WeatherActivity

--- app/src/main/java/com/tomislav/novacic/weathernovacic/WeatherActivity.kt
+++ app/src/main/java/com/tomislav/novacic/weathernovacic/WeatherActivity.kt
@@ -1,11 +1,11 @@
 package com.tomislav.novacic.weathernovacic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_weather)
     }
 }
